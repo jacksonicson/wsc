2b8aee91862d410fa0c4c684e675b93e3fa06c49
Andreas Wolke <jacksonicson@gmail.com>
2014-01-30 12:18:41 +0100

    Refactoring

--- l6/CXFWebDemo/src/main/java/beans/Test.java
+++
@@ -1,9 +0,0 @@
-package beans;
-
-public class Test {
-
-	public void sayHello() {
-		System.out.println("Test bean says hello!");
-	}
-
-}
--- l6/WSDLDemo/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>A simple Apache CXF Hello World application!</h2>
-</body>
-</html>
